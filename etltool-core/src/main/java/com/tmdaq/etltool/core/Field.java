e036c7aea0167ffca3968886f77cb4acbb6b8577
vttmlin <624204599@qq.com>
2018-05-31 21:20:01 +0800

    处女码

--- etltool-core/src/main/java/com/tmdaq/etltool/core/Field.java
+++ etltool-core/src/main/java/com/tmdaq/etltool/core/Field.java
@@ -21,34 +21,34 @@ public class Field {
         return src;
     }
 
-    public Field setSrc(String src) {
+    Field setSrc(String src) {
         this.src = src;
         return this;
     }
 
-    public String getDest() {
+    String getDest() {
         return dest;
     }
 
-    public Field setDest(String dest) {
+    Field setDest(String dest) {
         this.dest = dest;
         return this;
     }
 
-    public String getTypeHandle() {
+    String getTypeHandle() {
         return typeHandle;
     }
 
-    public Field setTypeHandle(String typeHandle) {
+    Field setTypeHandle(String typeHandle) {
         this.typeHandle = typeHandle;
         return this;
     }
 
-    public TypeHandler getTypeHandler() {
+    TypeHandler getTypeHandler() {
         return typeHandler;
     }
 
-    public Field setTypeHandler(TypeHandler typeHandler) {
+    Field setTypeHandler(TypeHandler typeHandler) {
         this.typeHandler = typeHandler;
         return this;
     }
